bfeff696eb33dd6a084ed8fb136c04736794f047
carlvoller <27472988+carlvoller@users.noreply.github.com>
2024-09-17 18:40:47 +0800

    some temporary changes

--- src/utils/diff.rs
+++ src/utils/diff.rs
@@ -1,17 +1,32 @@
-use std::any::TypeId;
+use std::{any::TypeId, cmp::min};
 
-use anyhow::{bail, Result};
-use ndarray::{ArrayBase, Data, Ix2, RawDataClone};
+use anyhow::{bail, Ok, Result};
+use ndarray::{s, Array, ArrayBase, Data, DataMut, Dimension, Ix1, Ix2, RawDataClone};
 
 use crate::errors::ArimaError;
 
 use super::sklearn::check_array;
 
-pub fn diff<D: Data<Elem = f64> + RawDataClone>(
-    arr: ArrayBase<D, Ix2>,
+fn _diff_vector<D: Data<Elem = f64> + DataMut>(
+    arr: &mut ArrayBase<D, Ix1>,
+    lag: u32,
+) -> Array<f64, Ix1> {
+    let n = arr.shape()[0];
+    let min_lag = min(lag as usize, n);
+
+    // [min_lag: n] - arr[: n-min_lag]
+    let operand1 = arr.slice(s![min_lag..n]);
+    let operand2 = arr.slice(s![..n - min_lag]);
+    let result = &operand1 - &operand2;
+
+    result
+}
+
+pub fn diff<S: Data<Elem = f64> + RawDataClone, D: Dimension>(
+    arr: Array<S, D>,
     lag: Option<u32>,
     differences: Option<u32>,
-) -> Result<ArrayBase<D, Ix2>> {
+) -> Result<Array<S, D>> {
     let lag_unwrapped = lag.unwrap_or(1);
     let differences_unwrapped = differences.unwrap_or(1);
 
@@ -29,10 +44,20 @@ pub fn diff<D: Data<Elem = f64> + RawDataClone>(
         })
     }
 
-    let x = check_array()
-        .arr(arr)
-        .dtype(TypeId::of::<f64>())
-        .call()?;
+    let x = check_array().arr(arr).dtype(TypeId::of::<f64>()).call()?;
 
-    Ok(x)
+    if x.ndim() == 1 {
+        let mut res = x.into_flat();
+
+        for _ in 1..differences_unwrapped {
+            res = _diff_vector(&mut res, lag_unwrapped);
+
+            if res.shape()[0] == 0 {
+                Ok(res)
+            }
+        }
+    } else if x.ndim() == 2 {
+    }
+
+    Ok(res)
 }
--- src/utils/sklearn/check_array.rs
+++ src/utils/sklearn/check_array.rs
@@ -2,21 +2,25 @@ use std::any::TypeId;
 
 use anyhow::{bail, Result};
 use bon::builder;
-use ndarray::{ArrayBase, Data, Dimension, RawDataClone};
+use ndarray::{Array, ArrayBase, Data, Dimension, RawDataClone};
 use num_traits::Float;
 
 use crate::errors::ArimaError;
 
-// TODO: Add ensure_min_samples, ensure_min_features support
 #[builder]
-pub fn check_array<T: 'static + Float, S: Data<Elem = T> + RawDataClone, D: Dimension>(
-    arr: ArrayBase<S, D>,
+pub fn check_array<T: 'static + Float, S: Data<Elem = T + RawDataClone> + RawDataClone, D: Dimension>(
+    arr: Array<S, D>,
     ensure_2d: Option<bool>,
     force_all_finite: Option<bool>,
     dtype: Option<TypeId>,
     copy: Option<bool>,
-) -> Result<ArrayBase<S, D>> {
-    let final_arr: ArrayBase<S, D> = {
+) -> Result<Array<S, D>> {
+    // Not exposing these options default options from sklearn
+    // as pmdarima always uses these default values
+    let ensure_min_samples = 1;
+    let ensure_min_features = 1;
+
+    let final_arr: Array<S, D> = {
         if copy.is_some_and(|x| x) {
             arr.clone()
         } else {
@@ -44,6 +48,29 @@ pub fn check_array<T: 'static + Float, S: Data<Elem = T> + RawDataClone, D: Dime
             bail!(ArimaError::ValueError {
                 expected: String::from("dtype to match ndarray type"),
                 found: String::from("dtype does not match ndarry type")
+            })
+        }
+    }
+
+    // I still do these checks despite ensure_min_samples and ensure_min_features being constant
+    // in the event that I choose to expose these parameters in the future.
+    if ensure_min_samples > 0 {
+        let shape = final_arr.shape();
+        if shape.len() < 1 || shape[0] < 1 {
+            bail!(ArimaError::ValueError {
+                expected: String::from("an array with > 0 samples"),
+                found: String::from("array with < 1 samples")
+            })
+        }
+    }
+
+    if ensure_min_features > 0 && final_arr.ndim() == 2 {
+        let shape = final_arr.shape();
+        let features = shape[1];
+        if features < ensure_min_features {
+            bail!(ArimaError::ValueError {
+                expected: format!("a 2d array with at least {ensure_min_features}"),
+                found: format!("an array with {features}")
             })
         }
     }
